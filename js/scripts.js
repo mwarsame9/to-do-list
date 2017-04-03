0a9d44b5ebc666d6624fbf4af296c4c62049e1f0
Jason Ainsworth and Mohamed Warsame <jason@>
2017-04-03 14:43:07 -0700

    create basic layout for todo list program. add in forms for user to input task name, task description, and due date. add in javascript functionality to display user task list.

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+function Tasks(taskName, description, dueDate) {
+  this.taskName = taskName;
+  this.description = description;
+  this.dueDate = dueDate;
+}
+
+
+
+
+$(document).ready(function() {
+  $(".form-one").submit(function(event) {
+    event.preventDefault();
+
+    var taskNameInput = $("input[name=taskName]").val();
+    var descriptionInput = $("input[name=description]").val();
+    var dueDateInput = $("#datepicker").val();
+
+    var newTask = new Tasks(taskNameInput, descriptionInput, dueDateInput);
+
+    $("#output").append("<li><span class='clickable'>" + newTask.taskName + "</span><button class='doneButton'>Complete</button></li><br></br>");
+
+    $("#output").show();
+
+    $(".clickable").last().click(function() {
+      $("#showOutput").show();
+      $("#taskName").text(newTask.taskName);
+      $("#description").text(newTask.description);
+      $("#dueDate").text(newTask.dueDate);
+    });
+
+    $("input[name=taskName]").val("");
+    $("input[name=description]").val("");
+    $("#datepicker").val("");
+
+
+    $(".doneButton").click(function(){
+      $(this).prev().toggleClass("strikeThrough");
+    });
+  });
+});
